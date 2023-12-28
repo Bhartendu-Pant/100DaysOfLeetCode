a03055498938268483ea1103fc1bbd042291efdf
Bhartendu Pant <bhartendupant809@gmail.com>
2023-12-28 10:41:48 +0530

    Create Day72 Remove Linked List Elements.cpp

---
+++ Day72 Remove Linked List Elements.cpp
@@ -0,0 +1,57 @@
+/*
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 104].
+1 <= Node.val <= 50
+0 <= val <= 50
+
+*/
+
+//Solution:
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head==NULL)return NULL;
+        ListNode* dummy=new ListNode(-1);
+        dummy->next=head;
+        ListNode* newHead=dummy;
+
+        while(newHead->next != NULL)
+        {
+            if(newHead->next->val==val)
+            newHead->next=newHead->next->next;
+            else
+            newHead=newHead->next;
+        }
+        return dummy->next;
+    }
+};
